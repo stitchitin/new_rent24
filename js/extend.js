b638a0fbaa863aea129e083a34472eb800430149
julietowah <julietowah@gmail.com>
2024-05-13 08:03:54 +0100

    new update

--- js/extend.js
+++ js/extend.js
@@ -8,4 +8,20 @@ function loadHTML(file, header) {
 }
 
 // Load header and footer
-loadHTML('unique/header.html', 'header');
+loadHTML('unique/header_2.html', 'header');
+
+
+
+
+
+function loadHTML(file, header2) {
+    fetch(file)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(header2).innerHTML = html;
+        })
+        .catch(error => console.error('Error loading HTML:', error));
+}
+
+// Load header and footer
+loadHTML('unique/adminheader.html', 'header2');
