d5020c943201eef2968eca374ab79e2951f1c612
stitchitin <stitchitin@gmail.com>
2024-07-09 14:54:56 +0100

    update rentitems

--- backend/rentItem.php
+++ backend/rentItem.php
@@ -13,22 +13,24 @@ function validateAndFormatDate($date) {
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Validate and sanitize inputs
+    // Read the incoming raw data
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
     if (
-        isset($_POST['paymentId']) && isset($_POST['startDate']) &&
-        isset($_POST['endDate']) && isset($_POST['quantity']) &&
-        isset($_POST['itemId']) && isset($_POST['userId']) &&
-        isset($_POST['vendorId']) && isset($_POST['totalPrice'])
+        isset($request->paymentId) && isset($request->startDate) &&
+        isset($request->endDate) && isset($request->quantity) &&
+        isset($request->itemId) && isset($request->userId) &&
+        isset($request->vendorId) && isset($request->totalPrice)
     ) {
         // Sanitize inputs to avoid code spam and injections
-        $paymentId = filter_var($_POST['paymentId'], FILTER_SANITIZE_STRING);
-        $startDate = filter_var($_POST['startDate'], FILTER_SANITIZE_STRING);
-        $endDate = filter_var($_POST['endDate'], FILTER_SANITIZE_STRING);
-        $quantity = filter_var($_POST['quantity'], FILTER_SANITIZE_NUMBER_INT);
-        $itemId = filter_var($_POST['itemId'], FILTER_SANITIZE_NUMBER_INT);
-        $userId = filter_var($_POST['userId'], FILTER_SANITIZE_NUMBER_INT);
-        $vendorId = filter_var($_POST['vendorId'], FILTER_SANITIZE_NUMBER_INT);
-        $totalPrice = filter_var($_POST['totalPrice'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $paymentId = filter_var($request->paymentId, FILTER_SANITIZE_STRING);
+        $startDate = filter_var($request->startDate, FILTER_SANITIZE_STRING);
+        $endDate = filter_var($request->endDate, FILTER_SANITIZE_STRING);
+        $quantity = filter_var($request->quantity, FILTER_SANITIZE_NUMBER_INT);
+        $itemId = filter_var($request->itemId, FILTER_SANITIZE_NUMBER_INT);
+        $userId = filter_var($request->userId, FILTER_SANITIZE_NUMBER_INT);
+        $vendorId = filter_var($request->vendorId, FILTER_SANITIZE_NUMBER_INT);
+        $totalPrice = filter_var($request->totalPrice, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 
         // Validate and format date inputs
         $formattedStartDate = validateAndFormatDate($startDate);
