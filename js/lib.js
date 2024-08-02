9f1f41b6ee99b2831687fda483e12345c7ef63d1
Ryan Zayne <phoenixastro97@gmail.com>
2024-08-02 20:40:08 +0100

    feat: Update API endpoint, modal title, and datepicker inputs
    This commit updates the API endpoint in the `ecom-product-details.js` file from "backend/rentitem.php" to "backend/rentItem.php" for consistency. It also updates the modal title in the `ecom-product-detail.html` file from "Modal title" to "Book for renting" for better clarity. Additionally, the commit adds the `required` attribute to the datepicker inputs in the `ecom-product-detail.html` file to ensure that the user must select a date before submitting the form.

--- js/lib.js
+++ js/lib.js
@@ -1,11 +1,11 @@
-export { callApi } from "https://esm.run/@zayne-labs/callapi@0.4.4";
+export { callApi } from "https://esm.run/@zayne-labs/callapi@0.4.6";
 
 export * as dateFns from "https://cdn.jsdelivr.net/npm/date-fns@3.6.0/+esm";
 
 import "https://cdn.jsdelivr.net/npm/sweetalert2@11";
 export { default as Swal } from "https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm";
 
-export const sweetAlert = Swal.fire.bind(Swal);
+export const sweetAlert = (value) => Swal.fire(value);
 
 const PromiseWithResolvers = () => {
 	let resolve, reject;
